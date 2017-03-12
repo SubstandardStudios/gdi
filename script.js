123711e00254880c0b8e6d90c8522792167bd14a
Cedric Hutchings <cedhut02@gmail.com>
2017-03-12 15:38:32 -0400

    Added console?
    I attempted to add a console that is embedded into the page.

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 
 //Start of function dictionary
 
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function () {
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+        } else {
+            logger.innerHTML += arguments[i] + '<br />';
+        }
+      }
+    }
+})();
+
 function printTest(){
 	console.log("This function prints 'Test!' for testing purposes. Test!");
 }
